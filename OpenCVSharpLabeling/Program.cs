49c992f8d786fba8b5ebd83c9a49c58612352635
jykim31337 <jykim31337@gmail.com>
2016-07-07 16:39:43 +0900

    160707-1639

--- OpenCVSharpLabeling/Program.cs
+++ OpenCVSharpLabeling/Program.cs
@@ -15,70 +15,59 @@ namespace OpenCVSharpLabeling
             Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
             Mat binary = gray.Threshold(0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
 
-            Mat label = new Mat();
-            int nLabels = Cv2.ConnectedComponents(binary, label, PixelConnectivity.Connectivity8, MatType.CV_32SC1);
+            ConnectedComponents cc = Cv2.ConnectedComponentsEx(binary);
 
-            Scalar[] colors = Enumerable.Range(0, nLabels + 1).Select(_ => Scalar.RandomColor()).ToArray();
-            colors[0] = Scalar.Black;
+            #region 전체 이미지에서 픽셀별로 어떤 라벨인지 확인
+            Mat Color = src.EmptyClone();
+            var colorIndexer = Color.GetGenericIndexer<Vec3b>();
+            Scalar[] colors = Enumerable.Range(0, cc.LabelCount).Select(_ => Scalar.RandomColor()).ToArray();
 
-            int rows = binary.Rows;
-            int cols = binary.Cols;
-            var dst = new Mat(rows, cols, MatType.CV_8UC3);
-            var labelIndexer = label.GetGenericIndexer<int>();
-            var dstIndexer = dst.GetGenericIndexer<Vec3b>();
-
-            for (int y = 0; y < rows; y++)
+            for (int y = 0; y < src.Height; y++)
             {
-                for (int x = 0; x < cols; x++)
+                for (int x = 0; x < src.Width; x++)
                 {
-                    int labelValue = labelIndexer[y, x];
-                    dstIndexer[y, x] = colors[labelValue].ToVec3b();
+                    int labelValue = cc.Labels[y, x];
+
+                    colorIndexer[y, x] = colors[labelValue].ToVec3b();
                 }
             }
 
-            Cv2.NamedWindow("dst", WindowMode.AutoSize);
+            Cv2.NamedWindow("전체이미지에서 픽셀별 라벨 인덱스 확인", WindowMode.AutoSize);
 
-            Cv2.ImShow("dst", dst);
+            Cv2.ImShow("전체이미지에서 픽셀별 라벨 인덱스 확인", Color);
+            #endregion
 
-            //ConnectedComponents cc = Cv2.ConnectedComponentsEx(binary);
 
-            //#region 전체 이미지에서 픽셀별로 어떤 라벨인지 확인
-            //Mat Color = src.EmptyClone();
-            //var colorIndexer = Color.GetGenericIndexer<Vec3b>();
-            //Scalar[] colors = Enumerable.Range(0, cc.LabelCount).Select(_ => Scalar.RandomColor()).ToArray();
+            #region 특정 라벨에 접근
+            Cv2.NamedWindow("특정 라벨 접근", WindowMode.AutoSize);
+            foreach (var blob in cc.Blobs.Skip(0))
+            {
+                src.Rectangle(blob.Rect, Scalar.Red);
+            }
+            Cv2.ImShow("특정 라벨 접근", src);
+            #endregion
 
-            //for (int y = 0; y < src.Height; y++)
-            //{
-            //    for (int x = 0; x < src.Width; x++)
-            //    {
-            //        int labelValue = cc.Labels[y, x];
+            #region 가장 큰 blob 찾기
+            //ConnectedComponents.Blob maxBlob1 = cc.Blobs.OrderByDescending(b => b.Area).First(); // 이렇게 하면 거의 배경이 나옴
+            ConnectedComponents.Blob maxBlob1 = cc.Blobs.Skip(1).OrderByDescending(b => b.Area).First(); //그래서 이렇게 하나를 스킵함
+            //위와 동일한 코드. 간결함
+            ConnectedComponents.Blob maxBlob2 = cc.GetLargestBlob();
 
-            //        colorIndexer[y, x] = colors[labelValue].ToVec3b();
-            //    }
-            //}
+            var filtered = new Mat();
+            //가장 큰 Blob 만 필터링하여 빈 이미지에 그리기
+            cc.FilterByBlob(src, filtered, maxBlob1);
 
-            //Cv2.NamedWindow("전체이미지에서 픽셀별 라벨 확인", WindowMode.AutoSize);
+            Cv2.NamedWindow("maxBlob2", WindowMode.AutoSize);
+            Cv2.ImShow("maxBlob2", filtered);
+            #endregion
 
-            //Cv2.ImShow("전체이미지에서 픽셀별 라벨 확인", Color);
-            //#endregion
+            #region 
+            var dst = new Mat();
+            cc.RenderBlobs(dst);
 
-            //Cv2.NamedWindow("특정 라벨 접근");
-            //foreach (var blob in cc.Blobs.Skip(0))
-            //{
-            //    src.Rectangle(blob.Rect, Scalar.Red);
-            //}
-            //Cv2.ImShow("특정 라벨 접근", src);
-
-            //ConnectedComponents.Blob maxBlob1 = cc.Blobs.Skip(1).OrderByDescending(b => b.Area).First();
-
-            //ConnectedComponents.Blob maxBlob2 = cc.GetLargestBlob(); // OpenCvSharp.Blob 상당의 편리한 메소드
-
-            //var dst = new Mat();
-            //cc.RenderBlobs(dst);
-
-            //var maxBlob = cc.GetLargestBlob();
-            //var filtered = new Mat();
-            //cc.FilterByBlob(src, filtered, maxBlob);
+            Cv2.NamedWindow("label 을 빈 Mat에 그리기", WindowMode.AutoSize);
+            Cv2.ImShow("label 을 빈 Mat에 그리기", dst);
+            #endregion
 
             int c = Cv2.WaitKey();
         }
