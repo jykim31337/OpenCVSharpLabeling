55e1777029ad1286e51210d7b5ce5d5d2bdc394d
jykim31337 <jykim31337@gmail.com>
2016-07-08 13:25:20 +0900

    160708-1325

--- OpenCVSharpLabeling/Program.cs
+++ OpenCVSharpLabeling/Program.cs
@@ -17,9 +17,9 @@ namespace OpenCVSharpLabeling
 
             ConnectedComponents cc = Cv2.ConnectedComponentsEx(binary);
 
-            #region 전체 이미지에서 픽셀별로 어떤 라벨인지 확인
-            Mat Color = src.EmptyClone();
-            var colorIndexer = Color.GetGenericIndexer<Vec3b>();
+            #region PointLabelIndex
+            Mat pixelLabel = src.EmptyClone();
+            var pixelLabelIndexer = pixelLabel.GetGenericIndexer<Vec3b>();
             Scalar[] colors = Enumerable.Range(0, cc.LabelCount).Select(_ => Scalar.RandomColor()).ToArray();
 
             for (int y = 0; y < src.Height; y++)
@@ -28,45 +28,43 @@ namespace OpenCVSharpLabeling
                 {
                     int labelValue = cc.Labels[y, x];
 
-                    colorIndexer[y, x] = colors[labelValue].ToVec3b();
+                    pixelLabelIndexer[y, x] = colors[labelValue].ToVec3b();
                 }
             }
 
-            Cv2.NamedWindow("전체이미지에서 픽셀별 라벨 인덱스 확인", WindowMode.AutoSize);
+            Cv2.NamedWindow("PointLabelIndex", WindowMode.AutoSize);
 
-            Cv2.ImShow("전체이미지에서 픽셀별 라벨 인덱스 확인", Color);
+            Cv2.ImShow("PointLabelIndex", pixelLabel);
             #endregion
 
 
-            #region 특정 라벨에 접근
-            Cv2.NamedWindow("특정 라벨 접근", WindowMode.AutoSize);
-            foreach (var blob in cc.Blobs.Skip(0))
+            #region Connect Labels
+            Cv2.NamedWindow("ConnectLabels", WindowMode.AutoSize);
+            foreach (var blob in cc.Blobs.Skip(1)) // Skip(1) mean background skip
             {
                 src.Rectangle(blob.Rect, Scalar.Red);
             }
-            Cv2.ImShow("특정 라벨 접근", src);
+            Cv2.ImShow("ConnectLabels", src);
             #endregion
 
-            #region 가장 큰 blob 찾기
-            //ConnectedComponents.Blob maxBlob1 = cc.Blobs.OrderByDescending(b => b.Area).First(); // 이렇게 하면 거의 배경이 나옴
-            ConnectedComponents.Blob maxBlob1 = cc.Blobs.Skip(1).OrderByDescending(b => b.Area).First(); //그래서 이렇게 하나를 스킵함
-            //위와 동일한 코드. 간결함
-            ConnectedComponents.Blob maxBlob2 = cc.GetLargestBlob();
+            #region Get Max Blob And Show
+            ConnectedComponents.Blob maxBlob = cc.Blobs.Skip(1).OrderByDescending(b => b.Area).First(); // Get Max Blob And Skip(1) mean background skip
+            ConnectedComponents.Blob maxBlobSimple = cc.GetLargestBlob(); //maxBlob and maxBlobSimple is same Blob
 
+            //Filtering Blob
             var filtered = new Mat();
-            //가장 큰 Blob 만 필터링하여 빈 이미지에 그리기
-            cc.FilterByBlob(src, filtered, maxBlob1);
+            cc.FilterByBlob(src, filtered, maxBlobSimple);
 
-            Cv2.NamedWindow("maxBlob2", WindowMode.AutoSize);
-            Cv2.ImShow("maxBlob2", filtered);
+            Cv2.NamedWindow("FilteringMaxBlobSimple", WindowMode.AutoSize);
+            Cv2.ImShow("FilteringMaxBlobSimple", filtered);
             #endregion
 
             #region 
             var dst = new Mat();
             cc.RenderBlobs(dst);
 
-            Cv2.NamedWindow("label 을 빈 Mat에 그리기", WindowMode.AutoSize);
-            Cv2.ImShow("label 을 빈 Mat에 그리기", dst);
+            Cv2.NamedWindow("RenderBlobs", WindowMode.AutoSize);
+            Cv2.ImShow("RenderBlobs", dst);
             #endregion
 
             int c = Cv2.WaitKey();
