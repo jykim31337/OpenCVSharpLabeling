669b4f52ab8b85c24c73013712bebd77d0def4fd
jykim31337 <jykim31337@gmail.com>
2016-07-08 16:22:15 +0900

    commant

--- OpenCVSharpLabeling/Program.cs
+++ OpenCVSharpLabeling/Program.cs
@@ -13,14 +13,17 @@ namespace OpenCVSharpLabeling
         {
             Mat src = new Mat("0.png", ImreadModes.Color);
             Mat gray = src.CvtColor(ColorConversionCodes.BGR2GRAY);
-            Mat binary = gray.Threshold(0, 255, ThresholdTypes.Otsu | ThresholdTypes.Binary);
+            Mat binary = gray.Threshold(0, 255, ThresholdTypes.Otsu | 
+                ThresholdTypes.Binary);
 
             ConnectedComponents cc = Cv2.ConnectedComponentsEx(binary);
 
             #region PointLabelIndex
+
             Mat pixelLabel = src.EmptyClone();
             var pixelLabelIndexer = pixelLabel.GetGenericIndexer<Vec3b>();
-            Scalar[] colors = Enumerable.Range(0, cc.LabelCount).Select(_ => Scalar.RandomColor()).ToArray();
+            Scalar[] colors = Enumerable.Range(0, cc.LabelCount).Select
+                (_ => Scalar.RandomColor()).ToArray();
 
             for (int y = 0; y < src.Height; y++)
             {
@@ -33,23 +36,30 @@ namespace OpenCVSharpLabeling
             }
 
             Cv2.NamedWindow("PointLabelIndex", WindowMode.AutoSize);
-
             Cv2.ImShow("PointLabelIndex", pixelLabel);
+
             #endregion
 
-
             #region Connect Labels
-            Cv2.NamedWindow("ConnectLabels", WindowMode.AutoSize);
-            foreach (var blob in cc.Blobs.Skip(1)) // Skip(1) mean background skip
+
+            // Skip(1) mean background skip
+            foreach (var blob in cc.Blobs.Skip(1)) 
             {
                 src.Rectangle(blob.Rect, Scalar.Red);
             }
+
+            Cv2.NamedWindow("ConnectLabels", WindowMode.AutoSize);
             Cv2.ImShow("ConnectLabels", src);
+
             #endregion
 
             #region Get Max Blob And Show
-            ConnectedComponents.Blob maxBlob = cc.Blobs.Skip(1).OrderByDescending(b => b.Area).First(); // Get Max Blob And Skip(1) mean background skip
-            ConnectedComponents.Blob maxBlobSimple = cc.GetLargestBlob(); //maxBlob and maxBlobSimple is same Blob
+
+            // Get Max Blob And Skip(1) mean background skip
+            ConnectedComponents.Blob maxBlob = cc.Blobs.Skip(1).
+                OrderByDescending(b => b.Area).First();
+            //maxBlob and maxBlobSimple is same Blob
+            ConnectedComponents.Blob maxBlobSimple = cc.GetLargestBlob(); 
 
             //Filtering Blob
             var filtered = new Mat();
@@ -57,14 +67,17 @@ namespace OpenCVSharpLabeling
 
             Cv2.NamedWindow("FilteringMaxBlobSimple", WindowMode.AutoSize);
             Cv2.ImShow("FilteringMaxBlobSimple", filtered);
+
             #endregion
 
-            #region 
+            #region RenderBlobs
+
             var dst = new Mat();
             cc.RenderBlobs(dst);
 
             Cv2.NamedWindow("RenderBlobs", WindowMode.AutoSize);
             Cv2.ImShow("RenderBlobs", dst);
+
             #endregion
 
             int c = Cv2.WaitKey();
